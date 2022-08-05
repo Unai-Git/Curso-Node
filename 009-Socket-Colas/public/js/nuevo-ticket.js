c45a4b26dad7f552b4b8ccc3818b2d9f4103fb27
Unai <unai_des@hotmail.com>
2022-08-05 19:56:30 +0200

    Colas finalizado

--- 009-Socket-Colas/public/js/nuevo-ticket.js
+++ 009-Socket-Colas/public/js/nuevo-ticket.js
@@ -1,30 +1,28 @@
-//Refs HTML
-const lblNuevoTicket = document.querySelector("#lblNuevoTicket");
-const btnCrear = document.querySelector("button");
+// Referencias HTML
+const lblNuevoTicket = document.querySelector('#lblNuevoTicket');
+const btnCrear = document.querySelector('button');
 
-//Socket
+
 const socket = io();
 
-socket.on("connect", () => {
-  // console.log('Conectado');
-  btnCrear.disabled = false;
+
+socket.on('connect', () => {
+    btnCrear.disabled = false;
 });
 
-socket.on("disconnect", () => {
-  // console.log('Desconectado del servidor');
-  btnCrear.disabled = true;
+socket.on('disconnect', () => {
+    btnCrear.disabled = true;
 });
 
-socket.on("enviar-mensaje", (payload) => {
-  console.log(payload);
-});
+socket.on('ultimo-ticket', (ultimo) => {
+    lblNuevoTicket.innerText = 'Ticket ' + ultimo;
+})
 
-socket.on("ultimo-ticket", (ultimo) => {
-  lblNuevoTicket.innerText = "Ticket: " + ultimo;
-});
 
-btnCrear.addEventListener("click", () => {
-  socket.emit("siguiente-ticket", null, (ticket) => {
-    lblNuevoTicket.innerText = ticket;
-  });
+btnCrear.addEventListener( 'click', () => {
+   
+    socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+        lblNuevoTicket.innerText = ticket;
+    });
+
 });
